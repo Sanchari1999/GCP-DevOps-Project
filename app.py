1bb17950153eca6ca2c4ca598fc4bc08cb4d594a
Sanchari1999 <95698881+Sanchari1999@users.noreply.github.com>
2023-04-24 19:38:31 +0530

    updating the code for the application

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-# Writing a flask application code
+# Writing a Flask application code
 
+# Import the Flask class from the flask module
 from flask import Flask
-app = Flask(_name_)
 
-# Base route of the application 
+# Create a new instance of the Flask class and name it "app"
+app = Flask(__name__)
 
+# Define the base route of the application using the app.route decorator
 @app.route('/')
 def hello_world():
+    # This function returns a string that says "Hello, Simple Flask Application"
     return 'Hello, Simple Flask Application'
